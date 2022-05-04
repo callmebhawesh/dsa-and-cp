de9a8ede3dd8a0cc0a8755d2ff84092ef2e45a34
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-04 20:32:02 +0530

    DownwardTriangle pattern

---
+++ Pattern Questions/DownwardTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DownwardTriangle {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int row = sc.nextInt();
+        for(int i=row; i>=0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
