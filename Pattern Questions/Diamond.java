5ecc4da1233d825e8f6c3066382d0d2d071c91f3
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-05 14:17:01 +0530

    Diamond shape created

--- Pattern Questions/Diamond.java
+++ Pattern Questions/Diamond.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Diamond{
     public static void main(String[] args){
-
-
-        int rows = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows.");
+        int rows = sc.nextInt();
 //        upper half
         for (int i = 1; i<=rows; i++) {
             for (int j = 1; j<=rows-i; j++){
                 System.out.print(" ");
             }
             for (int j = 1; j<=(2*i-1); j++){
-                System.out.print(" * ");
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -21,7 +21,7 @@ public class Diamond{
                 System.out.print(" ");
             }
             for (int j = 1; j<=(2*i-1); j++){
-                System.out.print(" * ");
+                System.out.print("*");
             }
             System.out.println();
         }
