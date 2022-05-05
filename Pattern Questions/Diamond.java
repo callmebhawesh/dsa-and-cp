902ebb899855c0df82e8ed0de9c160afad6d6977
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-05 14:05:40 +0530

    not a perfect diamond

--- Pattern Questions/Diamond.java
+++ Pattern Questions/Diamond.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class Diamond{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the total number of rows");
-        System.out.print("=> ");
-        int rows = sc.nextInt();
-        for(int i=1; i<=rows; i++){
-            for(int j=1; j<=i; j++){
+
+
+        int rows = 3;
+//        upper half
+        for (int i = 1; i<=rows; i++) {
+            for (int j = 1; j<=rows-i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j<=(2*i-1); j++){
                 System.out.print(" * ");
             }
             System.out.println();
         }
-        for(int i=rows; i>=0; i--){
-            for (int j = 1; j<=rows; j--){
+//        lower half
+        for (int i = rows; i>=1; i--) {
+            for (int j = 1; j<=rows-i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j<=(2*i-1); j++){
                 System.out.print(" * ");
             }
             System.out.println();
