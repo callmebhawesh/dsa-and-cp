78607b04b240cb878cbfc2cccd843429477f22bc
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-07 22:28:26 +0530

    few changes

--- Common Questions/Factorial.java
+++ Common Questions/Factorial.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
-public class Factorial{
-    public static void main(String[] args){
+public class Factorial {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try{
-System.out.println("");
-        } finally{
+        try {
+            System.out.println("Enter any number to find it's factorial");
+            int number = sc.nextInt();
+            for (int i = 1; i <= number; i++) {
+
+            }
+        } finally {
             sc.close();
         }
     }
