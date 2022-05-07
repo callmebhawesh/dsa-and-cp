116dfd85e4048a57827d0d5b1b8785e5d5bea522
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-07 22:14:56 +0530

    :bulb:

---
+++ Common Questions/Factorial.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Factorial{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        try{
+System.out.println("");
+        } finally{
+            sc.close();
+        }
+    }
+}
