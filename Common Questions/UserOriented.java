6fa797084335300d1130bd291d726f1ece4fd6d4
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-08 09:17:33 +0530

    added UserOriented.java

---
+++ Common Questions/UserOriented.java
@@ -0,0 +1,15 @@
+// write a program to enters the number till the users wants and at the end it should display the count of positive , negative, and zeros entered.
+
+import java.util.Scanner;
+
+public class UserOriented {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            
+        }
+        finally {
+            sc.close();
+        }
+    }
+}
